7484f744cfb4b6e3faf240f706229b12b2993ed7
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-28 00:39:02 +0600

    Create settings.gradle.kts

---
+++ examples/settings.gradle.kts
@@ -0,0 +1,24 @@
+rootProject.name = "grpc-kotlin-examples"
+
+// when running the assemble task, ignore the android & graalvm related subprojects
+if (startParameter.taskRequests.find { it.args.contains("assemble") } == null) {
+    include("protos", "stub", "stub-lite", "client", "native-client", "server", "stub-android", "android")
+} else {
+    include("protos", "stub", "server")
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        google()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.android.application") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+}
