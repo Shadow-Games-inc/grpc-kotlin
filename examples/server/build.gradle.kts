378fb87bb16ab1885fbd5ea13a842b1338d15d5a
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-28 00:38:19 +0600

    Create build.gradle.kts

---
+++ examples/server/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    application
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation(project(":stub"))
+    runtimeOnly("io.grpc:grpc-netty:${rootProject.ext["grpcVersion"]}")
+}
+
+tasks.register<JavaExec>("HelloWorldServer") {
+    dependsOn("classes")
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("io.grpc.examples.helloworld.HelloWorldServerKt")
+}
+
+tasks.register<JavaExec>("RouteGuideServer") {
+    dependsOn("classes")
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("io.grpc.examples.routeguide.RouteGuideServerKt")
+}
+
+tasks.register<JavaExec>("AnimalsServer") {
+    dependsOn("classes")
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("io.grpc.examples.animals.AnimalsServerKt")
+}
+
+val helloWorldServerStartScripts = tasks.register<CreateStartScripts>("helloWorldServerStartScripts") {
+    mainClass.set("io.grpc.examples.helloworld.HelloWorldServerKt")
+    applicationName = "hello-world-server"
+    outputDir = tasks.named<CreateStartScripts>("startScripts").get().outputDir
+    classpath = tasks.named<CreateStartScripts>("startScripts").get().classpath
+}
+
+val routeGuideServerStartScripts = tasks.register<CreateStartScripts>("routeGuideServerStartScripts") {
+    mainClass.set("io.grpc.examples.routeguide.RouteGuideServerKt")
+    applicationName = "route-guide-server"
+    outputDir = tasks.named<CreateStartScripts>("startScripts").get().outputDir
+    classpath = tasks.named<CreateStartScripts>("startScripts").get().classpath
+}
+
+val animalsServerStartScripts = tasks.register<CreateStartScripts>("animalsServerStartScripts") {
+    mainClass.set("io.grpc.examples.animals.AnimalsServerKt")
+    applicationName = "animals-server"
+    outputDir = tasks.named<CreateStartScripts>("startScripts").get().outputDir
+    classpath = tasks.named<CreateStartScripts>("startScripts").get().classpath
+}
+
+tasks.named("startScripts") {
+    dependsOn(helloWorldServerStartScripts)
+    dependsOn(routeGuideServerStartScripts)
+    dependsOn(animalsServerStartScripts)
+}
